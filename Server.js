68b5e61b592b13294755ec60f8dd7d7562ee1206
fernando.pinedo@outlook.com <fernando.pinedo@outlook.com>
2018-06-07 23:54:00 -0500

    Agrega permisos de cabecera

--- Server.js
+++ Server.js
@@ -25,6 +25,11 @@ app.use(bodyParser.urlencoded({'extended':'true'}));            // parse applica
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Connection
 mongoose.connect(database.url);
